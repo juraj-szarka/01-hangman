7dfabcea9a11eca39debd911b8ce7af44dc79e81
jsz <ju.szarka@gmail.com>
2025-07-14 00:22:29 +0200

    unrefined functional hangman

--- main.py
+++ main.py
@@ -15,24 +15,35 @@ class hangman:
         self.letter_to_guess = ''  # next tried letter
         self.game_is_active = True
         self.is_first_round = True
-
+        self.hang_word = self.get_random_word()
         self.start_game()
 
     def start_game(self):
         # Starts all the necesary function to make the game moving
 
-        self.hang_state = '_' * len(self.hang_word)
-        print(self.hang_state)
+
+        self.update_hang_state()
+        # print(self.hang_word)
+        # print(self.tried_letters)
         self.hang_letters = self.letter_scan(self.hang_word)
-        if self.current_mistakes < 10:
+        if '_' not in self.hang_state:
+            self.game_is_active = False
+            print("""
+            
+                    Y O U   W I N !
+            
+            """)
+        elif self.current_mistakes < 10:
             self.round(self.current_round)
         else:
             self.game_is_active = False
-            """
+            print(f"""
             
                     G A M E   O V E R
+
+                    corect word: {self.hang_word}
             
-            """
+            """)
 
         if self.game_is_active:
             self.start_game()
@@ -45,6 +56,7 @@ class hangman:
                 self.is_first_round = False
                 print(f'''
                     Welcome to the GAME!''')
+            print(f'mistakes: {self.current_mistakes}/10')
             print(f'{self.hang_state}')
             letter_memory = self.letter_check()
             self.update_hang_state(letter_memory)
@@ -53,6 +65,7 @@ class hangman:
 
     def letter_check(self):
         # gets players letter to guess, checks if correctly
+
         letter_memory = ''
         print(letter_memory)
 
@@ -60,23 +73,25 @@ class hangman:
             letter_memory = input('Enter your letter: ')
             if letter_memory in self.tried_letters:
                 print('E: You already tried this letter! Try a new one')
+                letter_memory = ''
             elif len(letter_memory) > 1:
                 print('E: You entered too many letters! Try again')
+                letter_memory = ''
+        self.tried_letters += letter_memory
         return letter_memory
         
-    def update_hang_state(self, letter):
+    def update_hang_state(self, letter='.'):
         hang_state_memory = ''
-        if letter in self.hang_letters:
+        if letter in self.hang_letters or letter=='.':
             for i in self.hang_word:
-                if i not in self.hang_letters:
+                if i not in self.tried_letters:
                     hang_state_memory += '_'
-                elif i in self.hang_letters:
+                else:
                     hang_state_memory += i
         else:
             print(f'Letter "{letter}" doesnt appear in this word, try again!')
+            self.current_mistakes += 1
         self.hang_state = hang_state_memory
-
-
 
     def letter_scan(self, word):
         # Creates list of words that are located in each word for hangman
@@ -87,7 +102,7 @@ class hangman:
                 local_memory.append(i)
         return local_memory
     
-    def get_random_word(self, dict):
+    def get_random_word(self, dict=word_dict.common_words):
         return random.choice(dict)
 
 game = hangman()
